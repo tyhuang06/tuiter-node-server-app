b4f6ce267da37675a60eb7da060a0ec3aa0476ad
Jamie Huang <tyhuang06@gmail.com>
2023-03-22 16:26:07 -0400

    a8 - completed 5

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import HelloController from './controllers/hello-controller.js';
 import UserController from './controllers/users/users-controller.js';
 import TuitController from './controllers/tuits/tuits-controller.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 TuitController(app);
