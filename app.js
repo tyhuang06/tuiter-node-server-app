99363b1cde8c2486ed0f1a10ebdd7d9f01843627
Jamie Huang <tyhuang06@gmail.com>
2023-03-22 15:35:23 -0400

    a6 - completed part5

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import HelloController from './controllers/hello-controller.js';
 import UserController from './controllers/users/users-controller.js';
+import TuitController from './controllers/tuits/tuits-controller.js';
 
 const app = express();
 
 app.use(express.json());
 
+TuitController(app);
 HelloController(app);
 UserController(app);
 
